30e59e85b5ef5556bf8115b030e8513114c2ce8c
devin t brown <toel3my@gmail.com>
2017-10-10 23:47:58 -0700

    added compute.instances().insert()

--- sandbox.py
+++ sandbox.py
@@ -6,7 +6,7 @@ import iso8601
 
 
 def genconf(A):
-    with open('defTemplate', 'r') as f:
+    with open('confTempl', 'r') as f:
         template = f.read()     
     conf = template.replace("PLACE IP HERE", A)
     with open('conf','w') as f:
@@ -14,6 +14,8 @@ def genconf(A):
 
 def getLongestRunningId( runningInstances , compute, projId, zone ):
     instances = list( runningInstances )
+    if instances == []:
+        return instances
         
     def getStartTime( instance ):
         filterString = '(targetId eq {})(operationType eq start)'.format( instance['id'] ) 
@@ -36,23 +38,78 @@ def getLongestRunningId( runningInstances , compute, projId, zone ):
 def stopInstance( longestName, compute, projId, zone ):
     compute.instances().stop( project = projId, zone = zone, instance = longestName ).execute()
     
+def makeLessServers( instances, compute, projId, zone, serverCount ):
+    longestName = getLongestRunningId( runningInsts , compute , projId , zone )
+    #check to make sure the there was at least one server running
+    if longestName != []:
+        stopInstance( longestName, compute, projId, zone )
+    
+
+def restConfig( serverName ):
+    return {
+        "name": serverName,
+        "machineType": "projects/linen-walker-178616/zones/us-central1-c/machineTypes/f1-micro",
+        "disks": [
+            {
+                "autoDelete": True,
+                "boot": True,
+                "initializeParams" : {
+                    "sourceImage" : "projects/linen-walker-178616/global/images/lab02-restserver",
+                }
+            }
+        ],
+        "serviceAccounts": [
+            {
+            "email": "default",
+            "scopes": [
+                "https://www.googleapis.com/auth/devstorage.read_only",
+                "https://www.googleapis.com/auth/logging.write",
+                "https://www.googleapis.com/auth/monitoring.write",
+                "https://www.googleapis.com/auth/servicecontrol",
+                "https://www.googleapis.com/auth/service.management.readonly",
+                "https://www.googleapis.com/auth/trace.append"
+            ]
+            }
+        ],
+        "metadata": {
+            "items" : [{
+                "key" : "startup-script",
+                "value" : "nohup sudo FLASK_APP=/opt/restserver/restserver.py flask run --host=0.0.0.0 --port=80 &"
+            }],
+        },
+        "networkInterfaces": [
+            {
+                "accessConfigs": [
+                    {
+                        "name": "External NAT",
+                        "type": "ONE_TO_ONE_NAT"
+                    }
+                ],
+                "network": "projects/linen-walker-178616/global/networks/default"
+            }
+        ]
+    }
+        
+        
+    
+   
+
 
 def main():
     
     compute = googleapiclient.discovery.build('compute','v1')
     projId = 'linen-walker-178616'
     zone = 'us-central1-c'
-    #all instances servers gathered
+    #gather all restservers
     instances = compute.instances().list( project = projId, zone = zone, filter = '(name eq restserver.*)' ).execute()
-    #if to many instances stop one sto filter all to RUNNING
-    runningInsts = filter( lambda x: x['status'] == 'RUNNING', instances['items'] )
+    #determine how many of these are running
+    #runningInsts = filter( lambda x: x['status'] == 'RUNNING', instances['items'] )
+    #runningCount = len( list( runningInsts ) )    
+    #makeLessServers( instances, compute , projId, zone, 1 )
 
-    longestName = getLongestRunningId( runningInsts , compute , projId , zone )
+    config = restConfig( "restserver-07" )
 
-    stopInstance( longestName, compute, projId, zone )
-
-
-
+    compute.instances().insert( project = projId , body = config , zone = zone ).execute()
 
     #operations = compute.zoneOperations().list( project = projId, zone = zone ,filter = '(targetId eq 7746285348118427911)(operationType eq start)').execute() 
     #print( sorted([ opp['startTime'] for opp in operations['items'] ]))
@@ -61,44 +118,36 @@ def main():
 
 
 
-    '''
-    icount = curNumIns()
-    # x : + - instance counter, (1,x) : reduce instances, 
-    # (2,x) : create instances, 0 : do nothing
-    state = getState()
-
-    # *** When to generate confTempl ***
-    # the info for the confTempl 
-    ipList = ipBuilder()
-    genconf(ipList)
-
-    if state == 0:
-        #Do nothing?
-    elif state == 1:
-        #find the longest running instance
-        longestID = getLongestID()
-        #stop this instance
-        stopInstance(longestID)
-        while(instance counter != 0)
-            nextId = getNextRunningId()
-            stop(nexId)
-            instance counter --
-        placeIdHere = 
-    elif state == 2:
-        #start instance from existing copies
-        while(instance counter != 0)
-            #returns 0 if no copies available 
-            nextId = getNextAvailableID()
-            if( nextID == 0 )
-        #check instance count
-        #if instance count still not at 0 creat new instances
-        #after creating then start
     
-    # this function needs to ssh into the loadbalancer server
-    # in order to run < sudo service nginx reload > 
-    nGinxReload()
-
-    
-    
-    '''
 main() 
+
+
+'''
+ {
+  "canIpForward": false,
+  "cpuPlatform": "Unknown CPU Platform",
+  "creationTimestamp": "2017-09-15T10:08:07.968-07:00",
+  "description": "",
+ 
+  "id": "4343228110604449304",
+  "kind": "compute#instance",
+  "labelFingerprint": "42WmSpB8rSM=",
+ 
+  "scheduling": {
+    "automaticRestart": true,
+    "onHostMaintenance": "MIGRATE",
+    "preemptible": false
+  },
+  "selfLink": "projects/linen-walker-178616/zones/us-central1-c/instances/restserver-0",
+  "startRestricted": false,
+  "status": "TERMINATED",
+  "tags": {
+    "fingerprint": "6smc4R4d39I=",
+    "items": [
+      "http-server",
+      "https-server"
+    ]
+  },
+ 
+}
+'''
